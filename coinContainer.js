ed4be6cd295f4f057be5652abd33feeed71fa695
thekesslereffect <eldron2323@gmail.com>
2019-05-19 13:23:29 -0700

    creating coinContainer to rewrite the entire html to be dynamic

---
+++ coinContainer.js
@@ -0,0 +1,17 @@
+var index;
+var a = ["a", "b", "c", "etc."]; //Get all arrays from sheets
+for (index = 0; index < a.length; ++index) {
+	
+	// Create Grid item
+	// Create icon, "name from array" + .png
+	// Create name, "name from array"
+	// Create 24 shape, id="name from array"
+	// Create Dst shape, id="name from array"
+	// Get value of 24 from sheets
+	// Get value of Dst from sheets
+	// Change color based on 24
+	// Change color based on Dst
+
+    console.log(a[index]);
+
+}
